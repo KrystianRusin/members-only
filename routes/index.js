57e9cdfc5ab6f83e89474b39bcd19b6536346edc
KrystianRusin <krystian.r@hotmail.com>
2024-02-09 13:30:25 -0500

    styling added

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,11 @@ const router = express.Router();
 router.get("/", messageController.getMessage);
 
 router.get("/sign-up", function (req, res, next) {
-  res.render("sign-up", { title: "Sign Up" });
+  res.render("sign-up", { title: "Sign Up", user: req.user });
 });
 
 router.get("/log-in", function (req, res, next) {
-  res.render("log-in", { title: "Log In" });
+  res.render("log-in", { title: "Log In", user: req.user });
 });
 
 router.post("/create-message", messageController.createMessage);
